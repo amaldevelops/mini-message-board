706e3d1d66f5d365ebc1bfad8acb38d21ae756dd
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-12-17 12:01:35 +1100

    Added code to capture req body and add to messages

--- app.js
+++ app.js
@@ -1,31 +1,45 @@
-// App setup
+// Setup Express
 const express = require("express");
-const path = require("node:path");
 const app = express();
-const assetsPath = path.join(__dirname, "public");
-const newMessageRouter=require("./routes/newMessage");
 
+// Setup Router
+const { Router } = require("express");
+const newMessageRouter = Router();
+
+// Set Static asset path for serving images, css, etc.
+const path = require("node:path");
+const assetsPath=path.join(__dirname,"public");
 app.use(express.static(assetsPath));
+
+// Set view Engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use("/new",newMessageRouter)
 
+// Setup app level Express middleware called express.urlencoded() to parse the form data into req.body
+app.use(express.urlencoded({ extended: true }));
+
+// Set Application port ie. localhost:3000
 const PORT = 3000;
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Maverick",
+    text: "Welcome to Star Trek Next Generation!",
+    user: "Captain Picard",
     added: new Date(),
   },
   {
-    text: "Hello World!",
-    user: "Charles",
+    text: "Thanks captain !",
+    user: "Jodie",
     added: new Date(),
   },
   {
-    text: "Welcome to Melbourne !",
-    user: "Albanese",
+    text: "Take charge of the Ships Engineering and report back to me on the status of all systems!",
+    user: "Captain Picard",
+    added: new Date(),
+  },
+  {
+    text: "Yes Captain !",
+    user: "Jodie",
     added: new Date(),
   },
 ];
@@ -34,9 +48,16 @@ app.get("/", (req, res) => {
   res.render("index", { messages: messages });
 });
 
-// app.get("/new", (req, res) => {
-//   res.render("new");
-// });
+app.get("/new", (req, res) => {
+  res.render("form");
+});
+
+app.post("/new", (req, res) => {
+  const { messageText, authorName } = req.body;
+  messages.push({ text: messageText, user: authorName, added: new Date() });
+
+  res.redirect("/");
+});
 
 app.listen(PORT, () => {
   console.log("Mini Message Board");
