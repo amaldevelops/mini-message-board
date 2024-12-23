8039dbd37aab50b865e64019938393da62d89cf7
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-12-23 19:09:49 +1100

    Refactored Routes to a separate file

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express = require("express");
 const app = express();
 
 // Setup Router
-const { Router } = require("express");
-const newMessageRouter = Router();
+const messagesRouter = require ("./routes/messagesRouter");
 
 // Set Static asset path for serving images, css, etc.
 const path = require("node:path");
@@ -21,51 +20,32 @@ app.use(express.urlencoded({ extended: true }));
 // Set Application port ie. localhost:3000
 const PORT = 3000;
 
-const messages = [
-  {
-    text: "Welcome to Star Trek Next Generation!",
-    user: "Captain Picard",
-    added: new Date(),
-  },
-  {
-    text: "Thanks captain !",
-    user: "Jodie",
-    added: new Date(),
-  },
-  {
-    text: "Take charge of the Ships Engineering and report back to me on the status of all systems!",
-    user: "Captain Picard",
-    added: new Date(),
-  },
-  {
-    text: "Yes Captain !",
-    user: "Jodie",
-    added: new Date(),
-  },
-];
+app.use("/", messagesRouter)
 
-app.get("/", (req, res) => {
-  res.render("index", { messages: messages });
-});
 
-app.get("/new", (req, res) => {
-  res.render("form");
-});
 
-app.post("/new", (req, res) => {
-  const { messageText, authorName } = req.body;
-  messages.push({ text: messageText, user: authorName, added: new Date() });
+// app.get("/", (req, res) => {
+//   res.render("index", { messages: messages });
+// });
 
-  res.redirect("/");
-});
+// app.get("/new", (req, res) => {
+//   res.render("form");
+// });
 
-app.get("/:messageID", (req, res) => {
-  const { messageID } = req.params;
-  res.send(`Author: ${messages[messageID]["user"]} <br>
-        Message: ${messages[messageID]["text"]} <br>
-        Time: ${messages[messageID]["added"]} <br>
-        <-- Click the browser Back button to go back`);
-});
+// app.post("/new", (req, res) => {
+//   const { messageText, authorName } = req.body;
+//   messages.push({ text: messageText, user: authorName, added: new Date() });
+
+//   res.redirect("/");
+// });
+
+// app.get("/:messageID", (req, res) => {
+//   const { messageID } = req.params;
+//   res.send(`Author: ${messages[messageID]["user"]} <br>
+//         Message: ${messages[messageID]["text"]} <br>
+//         Time: ${messages[messageID]["added"]} <br>
+//         <-- Click the browser Back button to go back`);
+// });
 
 app.listen(PORT, () => {
   console.log(
