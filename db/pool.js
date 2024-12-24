8c06d0096326e82aea05466ef32d2c0bb4371282
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2024-12-24 17:11:28 +1100

    Add connection URI to the database

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,18 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
+// module.exports = new Pool({
+//   host: process.env.HOST_NAME,
+//   port: process.env.PORT,
+//   database: process.env.DATABASE_NAME,
+//   user: process.env.USER_NAME,
+//   password: process.env.PW,
+//   ssl:false,
+// });
+
+
+// Again, this should be read from an environment variable
 module.exports = new Pool({
-  host: process.env.HOST_NAME,
-  port: process.env.PORT,
-  database: process.env.DATABASE_NAME,
-  user: process.env.USER_NAME,
-  password: process.env.PW,
-  ssl:false,
+  connectionString: process.env.CONNECTION_URI
 });
+
